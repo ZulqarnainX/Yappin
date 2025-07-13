02b4164d2c7a690971491b31ae4d0987ab6d5b00
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-07-12 20:27:27 -0700

    fixed Navbar component name file

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 import {
   ClerkProvider,
   SignInButton,
@@ -27,13 +28,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-        {children}
-      </body>
-    </html>
-  </ClerkProvider>
+          <Navbar/>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
