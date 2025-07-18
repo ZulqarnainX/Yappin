58896296842554e228837983ab6d00b045d89bb0
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-07-18 02:09:21 -0700

    added links to forum button

--- app/forums/page.js
+++ app/forums/page.js
@@ -1,37 +1,44 @@
 import React from 'react'
 import Image from 'next/image';
 import { MessageCircle, Users, Zap, ArrowRight } from 'lucide-react'; // Importing icons from lucide-react
+import Link from 'next/link';
 
 const topics = [
   {
     text: "Python",
     img: "/python.jpg",
     desc: "Let’s get into Python development — whether you enjoy writing scripts, building bots, automating tasks, or using powerful tools like Django, Flask, or Pandas.",
+    slug: "python-discuss"
   },
   {
     text: "Frontend Development",
     img: "/frontend.jpg",
     desc: "Discuss everything UI-focused — from HTML and CSS to React, Next.js, animations, and building beautiful, responsive user interfaces.",
+    slug: "frontend-discuss"
   },
   {
     text: "Community Projects",
     img: "/community.jpg",
     desc: "Show what you’re working on, team up with others, help out with open-source projects, or get feedback from other creators in our community.",
+    slug: "community-projects"
   },
   {
     text: "Freelancing & Career",
     img: "/freelancing.jpg",
     desc: "Ask for portfolio reviews, share job hunting tips, or discuss freelance life, rates, and client experiences.",
+    slug: "freelancing-career"
   },
   {
     text: "Gaming & Esports",
     img: "/gaming.jpg",
     desc: "Whether you're into chill game nights or intense PvP and esports — connect with other gamers, share your experiences, and find teammates to squad up with.",
+    slug: "gaming-esports"
   },
   {
     text: "Anime & Fandoms",
     img: "/anime.jpg",
     desc: "Join conversations about anime, manga, fan theories, new releases, and your favorite fandoms. Share your anime list or fan art.",
+    slug: "anime-fandoms"
   },
 ];
 
@@ -49,10 +56,13 @@ const Forums = () => {
           <Image alt={topic.text} src={topic.img} width={80} height={80} className='mb-4 object-contain rounded-full' /> 
           <h2 className='text-2xl font-semibold text-gray-800 mb-2'>{topic.text}</h2> 
           <p className='text-gray-600 mb-4 flex-grow'>{topic.desc}</p> 
+          
+          <Link href={`/chat/${topic.slug}`}>
           <button className="group px-4 py-2 rounded-full bg-[#3f4ef3] font-semibold text-white text-[17px] shadow-xl hover:bg-blue-700 transition duration-300 flex items-center space-x-3 cursor-pointer">
               <span>Discuss Now</span>
               <ArrowRight className="w-6 h-6 transform group-hover:translate-x-1 transition-transform duration-300" />
             </button>
+          </Link>
         </div>
       ))}
     </div>
