3c86efe1a588fe7df3ee68b8d586965e55b55545
ZulqarnainX <syed.zulqarnain557@gmail.com>
2025-07-24 10:59:10 -0700

    feat: add ChatForum component and integrate currentUser for user context

--- app/forum/[slug]/page.js
+++ app/forum/[slug]/page.js
@@ -1,7 +1,10 @@
 
 import ChatForum from '@/components/ChatForum';
+import { currentUser } from '@clerk/nextjs/server'
+
 
 export default async function Page({ params }) {
+    const user = await currentUser()
     const slug = (await params).slug
     return <ChatForum slug={slug}/>
 }
---
+++ components/ChatForum.js
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+
+const ChatForum = ({clerkUser, slug}) => {
+  return (
+    <div>
+      ChatForum
+    </div>
+  )
+}
+
+export default ChatForum
